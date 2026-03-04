6f9d29caeab68f87803687f4568c7b1d1392edc1
Your Name <you@example.com>
2026-03-04 17:33:47 +0900

    first commit

---
+++ section03/src/app/search/layout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+export default function Layout({ children }: { chuildren: ReactNode }) {
+  return (
+    <div>
+      <div>임시gggggg</div>
+      {children}
+    </div>
+  );
+}
