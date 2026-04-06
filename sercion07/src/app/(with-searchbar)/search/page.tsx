7b15bf6e674adb0330ae5ef250c8e5a903266088
Your Name <jk120926@gmail.com>
2026-04-06 13:31:20 +0900

    7.1) server action

---
+++ sercion07/src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,39 @@
+import { BookData } from "@/types";
+import BookItem from "@/components/book-item";
+import { delay } from "@/util/delay";
+import { Suspense } from "react";
+
+async function SearchjResult({ q }: { q: string }) {
+  await delay(1500); //1.5초 딜레이
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
+    { cache: "force-cache" },
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다..</div>;
+  }
+
+  //JSON화면 렌더
+  const books: BookData[] = await response.json();
+
+  //화면 출력
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { q?: string };
+}) {
+  return (
+    <Suspense key={searchParams.q || ""} fallback={<div>loading...</div>}>
+      <SearchjResult q={searchParams.q || ""}></SearchjResult>
+    </Suspense>
+  );
+}
---
+++ sercion07/src/app/layout.tsx
@@ -0,0 +1,45 @@
+import "./globals.css";
+import Link from "next/link";
+import style from "./layout.module.css";
+import { BookData } from "@/types";
+
+async function Footer() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    { cache: "force-cache" },
+    // 하나라도 다이나믹 페이지가 있으면 다른 페이지도 다이나믹 페이지가 됨. force-cache를 사용하면 정적페이지로써 사용가능
+  );
+  if (!response.ok) {
+    return <footer>제작 @winterlood</footer>;
+  }
+
+  const books: BookData[] = await response.json();
+  const bookCnt = books.length;
+
+  return (
+    <footer>
+      <div>제작 @winterlood</div>
+      <div>{bookCnt}개의 도서가 등록되어 있습니다.</div>
+    </footer>
+  );
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <div className={style.container}>
+          <header>
+            <Link href={"/"}>📚 ONEBITE BOOKS</Link>
+          </header>
+          <main>{children}</main>
+          <Footer></Footer>
+        </div>
+      </body>
+    </html>
+  );
+}
