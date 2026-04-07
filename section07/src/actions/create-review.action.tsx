46893721c74cf85cc180804aba90a0101797ddf5
Your Name <jk120926@gmail.com>
2026-04-07 14:08:29 +0900

    7.4) 리뷰 재검증 구현

--- section07/src/actions/create-review.action.tsx
+++ section07/src/actions/create-review.action.tsx
@@ -1,5 +1,7 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+
 export async function createReviewAction(formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
@@ -13,6 +15,9 @@ export async function createReviewAction(formData: FormData) {
       { method: "POST", body: JSON.stringify({ bookId, content, author }) },
     );
     console.log(response.status);
+    revalidatePath(`/book/${bookId}`);
+    //revalidatePath는 /book/${bookId} 페이지에 해당하는 캐시를 무효화
+    //다음에 누가 그 페이지를 요청하면 서버에서 다시 fetch+렌더링됨
   } catch (err) {
     console.error(err);
   }
