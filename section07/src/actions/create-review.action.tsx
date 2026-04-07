430bb501e1441e66b61904f86a57960a60fae53d
Your Name <jk120926@gmail.com>
2026-04-07 14:38:13 +0900

    7.5) 다양한 재검증 방식

--- section07/src/actions/create-review.action.tsx
+++ section07/src/actions/create-review.action.tsx
@@ -1,6 +1,6 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
+import { revalidatePath, revalidateTag } from "next/cache";
 
 export async function createReviewAction(formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
@@ -15,9 +15,26 @@ export async function createReviewAction(formData: FormData) {
       { method: "POST", body: JSON.stringify({ bookId, content, author }) },
     );
     console.log(response.status);
-    revalidatePath(`/book/${bookId}`);
-    //revalidatePath는 /book/${bookId} 페이지에 해당하는 캐시를 무효화
-    //다음에 누가 그 페이지를 요청하면 서버에서 다시 fetch+렌더링됨
+
+    /** 
+     * revalidatePath
+    : revalidatePath는 /book/${bookId} 페이지에 해당하는 캐시를 무효화
+      다음에 누가 그 페이지를 요청하면 서버에서 다시 fetch+렌더링됨
+    */
+    // 1. 특정 주소의 해당하는 페이지만 재검증
+    // revalidatePath(`/book/${bookId}`);
+
+    // 2. 특정 경로의 모든 동적 페이지를 재검증
+    // revalidatePath("/book/[id]", "page");
+
+    // 3. 특정 레이아웃을 갖는 모든 페이지를 재검증
+    // revalidatePath("/(with-searchbar)", "layout");
+
+    // 4. 모든 데이터를 재검증
+    // revalidatePath("/", "layout");
+
+    // 5. 태그 기준, 데이터 캐시 재검증
+    revalidateTag(`review-${bookId}`); //페이지 전체가 아닌, 특정 fetch만 무효화
   } catch (err) {
     console.error(err);
   }
--- section07/src/app/book/[id]/page.tsx
+++ section07/src/app/book/[id]/page.tsx
@@ -44,6 +44,7 @@ async function BookDetail({ bookId }: { bookId: string }) {
 async function ReviewList({ bookId }: { bookId: string }) {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/book/${bookId}`,
+    { next: { tags: [`review-${bookId}`] } },
   );
 
   if (!response.ok) {
